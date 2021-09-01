8207a06ff67f8118e2f4d2dd057dda165e77f932
peng.tian <peng.tian@thoughtworks.com>
2021-09-01 14:17:40 +0800

    implement simple dependency with fusheng

--- src/test/java/com/tp/di/fusheng/ContainerTest.java
+++ src/test/java/com/tp/di/fusheng/ContainerTest.java
@@ -2,6 +2,8 @@ package com.tp.di.fusheng;
 
 import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
 import com.tp.di.Container;
+import com.tp.di.testDummies.Car;
+import com.tp.di.testDummies.Engine;
 
 @FuShengTest
 public class ContainerTest {
@@ -14,5 +16,20 @@ public class ContainerTest {
 
     public String createStringFromNew() {
         return new String();
+    }
+
+    public String createCarFromContainer() {
+        Container container = new Container();
+        container.addComponent(Engine.class);
+        container.addComponent(Car.class);
+
+        Car car = container.getInstance(Car.class);
+        return car.getEngine().start();
+    }
+
+    public String createCarFromNew() {
+        Engine engine = new Engine();
+        Car car1 = new Car(engine);
+        return car1.getEngine().start();
     }
 }
--- src/test/java/com/tp/di/testDummies/Engine.java
+++ src/test/java/com/tp/di/testDummies/Engine.java
@@ -4,5 +4,4 @@ public class Engine {
     public String start() {
         return "started";
     }
-
 }
